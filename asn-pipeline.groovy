9a76f939bdcf3dd026bd490c796c3f436a3dad7f
bootcampmk-rldatix <asencho.jovanov@rldatix.com>
2023-07-28 16:57:13 +0200

    Update Jenkinsfile

--- asn-pipeline.groovy
+++ asn-pipeline.groovy
@@ -1,18 +1,28 @@
 pipeline {
-    agent any
-
+    agent none
+    environment {
+        NPM_AUTH = credentials('npm-nexus')
+        NUGET_AUTH = credentials('nuget-nexus')
+    }
     stages {
-        stage('Build') {
-            steps {
-                // Your build steps here
-                sh 'npm install'
-                sh 'npm run build'
-            }
-        }
-        stage('Test') {
-            steps {
-                // Your test steps here
-                sh 'npm test'
+        stage('Install Packages') {
+            parallel {
+                stage('Install NPM') {
+                    agent {
+                        label "npm"
+                    }
+                    steps {
+                        sh 'npm install'
+                    }
+                }
+                stage('Install Nuget') {
+                    agent {
+                        label "nuget"
+                    }
+                    steps {
+                        sh 'nuget install'
+                    }
+                }
             }
         }
     }
